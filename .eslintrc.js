e80e1ec4738cc8fd10e82968a75b629072e821b4
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-03-25 14:24:05 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "google",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "airbnb-base",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "require-jsdoc": 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# JStemplate
